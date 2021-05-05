57c45ba0172941dd05605d68d8131dad3f33882d
Iulian Rotaru <iulian@gnotis.ro>
2021-05-05 12:43:24 +0300

    Create HTTP RMI client class for convenient use of HttpRmiTransaction.

---
+++ src/main/java/js/net/client/HttpRmiClient.java
@@ -0,0 +1,92 @@
+package js.net.client;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import js.lang.BugError;
+
+public class HttpRmiClient {
+	private final HttpRmiTransaction transaction;
+	private final String className;
+
+	public HttpRmiClient(String implementationURL, String className) {
+		this.transaction = new HttpRmiTransaction(new DefaultConnectionFactory(), implementationURL);
+		this.transaction.setReturnType(Void.class);
+		this.className = className;
+	}
+
+	/** @see HttpRmiTransaction#setReturnType(Type) */
+	public void setReturnType(Type returnType) {
+		transaction.setReturnType(returnType);
+	}
+
+	/** @see HttpRmiTransaction#setExceptions(Class[]) */
+	public void setExceptions(Class<?>... exceptions) {
+		transaction.setExceptions(exceptions);
+	}
+
+	public <T> T invoke(String methodName, Object... arguments) throws Exception {
+		transaction.setMethod(className, methodName);
+		transaction.setArguments(arguments);
+		return transaction.exec(null);
+	}
+
+	private static class DefaultConnectionFactory extends ConnectionFactory {
+		@Override
+		public HttpURLConnection openConnection(URL url) throws IOException {
+			if (!isSecure(url.getProtocol())) {
+				return (HttpURLConnection) url.openConnection();
+			}
+
+			TrustManager tm = new X509TrustManager() {
+				@Override
+				public void checkClientTrusted(java.security.cert.X509Certificate[] arg0, String arg1) throws java.security.cert.CertificateException {
+				}
+
+				@Override
+				public void checkServerTrusted(java.security.cert.X509Certificate[] arg0, String arg1) throws java.security.cert.CertificateException {
+				}
+
+				@Override
+				public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+					return null;
+				}
+			};
+
+			SSLContext sslContext;
+			try {
+				sslContext = SSLContext.getInstance("TLS");
+				sslContext.init(null, new TrustManager[] { tm }, null);
+				HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+				connection.setSSLSocketFactory(sslContext.getSocketFactory());
+				return connection;
+			} catch (Throwable e) {
+				new IOException(e);
+			}
+
+			// make compiler happy; we never step here
+			return null;
+		}
+	}
+
+	private static final String HTTP = "http";
+
+	private static final String HTTPS = "https";
+
+	private static boolean isSecure(String protocol) throws BugError {
+		if (HTTP.equalsIgnoreCase(protocol)) {
+			return false;
+		} else if (HTTPS.equalsIgnoreCase(protocol)) {
+			return true;
+		} else {
+			throw new BugError("Unsupported protocol |%s| for HTTP transaction.", protocol);
+		}
+	}
+}
